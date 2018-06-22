89a44f41c43fee358d391728e13b1c0459c50984
Rafael Riegel <Rafael Riegel@191.52.29.54>
2018-06-22 18:57:33 -0300

    Metodos D e F

--- src/Operaçoes/Rafael.java
+++ src/Operaçoes/Rafael.java
@@ -7,12 +7,12 @@ public class Rafael {
 	
 	
 	private Dentista dentistas[];
-	private int indiceDentistas=0;
+	private int indiceDentistas=1;
 	
 	public void listarDentista() {
 		for(int p = 0;  p < indiceDentistas; p++) {
 			if(dentistas[p] != null) {
-				System.out.println(p + " " + dentistas[p]);
+				System.out.println(p + " " + dentistas[p].getNome());
 			}
 		}
 	}
@@ -20,13 +20,15 @@ public class Rafael {
 		Rafael metodos2 = new Rafael();
 		metodos2.listarDentista();
 	}
+	
+	//Arrumar Metodo Fatura
 	private Fatura faturas[];
 	private int indiceFaturas=0;
 	
 	public void listarFatura() {
 		for(int v = 0;  v < indiceFaturas; v++) {
 			if(faturas[v] != null) {
-				System.out.println(v + " " + faturas[v]);
+				System.out.println(v + " " + faturas[v].getValor());
 				
 				
 			}
