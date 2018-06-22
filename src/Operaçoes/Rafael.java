0f132ab201c37a8d89ac7e4046af917047e9ce08
Rafael Riegel <Rafael Riegel@191.52.29.54>
2018-06-22 18:13:39 -0300

    metodo listar Dentista

---
+++ src/Operaçoes/Rafael.java
@@ -0,0 +1,15 @@
+package src.Operaçoes;
+
+public class Rafael {
+	
+	
+public void listaDentista() {
+	
+}
+
+public static void main(String args[]) {
+	Rafael metodos2 = new Rafael();
+	metodos2.listaDentista(); 
+}
+}
+
