cbbae43d11f3bf0a7e382456dabe6750d4dae940
Andrea Soares <Andrea Soares@Andrea>
2018-06-22 18:51:07 -0300

    implementação do metodo listarCliente

--- src/Operaçoes/Metodos.java
+++ src/Operaçoes/Metodos.java
@@ -5,12 +5,12 @@ import src.Operaçoes.*;
 public class Metodos {
 
 	private Cliente clientes[];
-	private int indiceClientes=0;
+	private int indiceClientes=1;
 	
 	public void listarCliente() {
 		for(int i = 0;  i < indiceClientes; i++) {
 			if(clientes[i] != null) {
-				System.out.println(i + " " + clientes[i]);
+				System.out.println(i + " " + clientes[i].getNome());
 			}
 		}
 	}
