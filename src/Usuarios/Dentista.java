279452a8086a6aa01603f4f6fd1c42a9821ca091
Andrea Soares <Andrea Soares@Andrea>
2018-06-22 17:28:12 -0300

    # WARNING: head commit changed in the meantime
    Extenção na classe Dentista para classe Pessoa e metodo editar

--- src/Usuarios/Dentista.java
+++ src/Usuarios/Dentista.java
@@ -2,48 +2,25 @@ package src.Usuarios;
 
 import java.util.Scanner;
 
-public class Dentista extends Pessoa{
+public class Dentista extends Pessoa {
 
-	private int Id_Dentista;
-	private String Nome;
-	private String CPF;
 	private String Especialidade;
 	
 	public Dentista() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public int getId_Dentista() {
-		return Id_Dentista;
-	}
-	public void setId_Dentista(int id_Dentista) {
-		Id_Dentista = id_Dentista;
-	}
 	public String getEspecialidade() {
 		return Especialidade;
 	}
 	public void setEspecialidade(String especialidade) {
 		Especialidade = especialidade;
 	}
-	public String getNome() {
-		return Nome;
+	
+	@Override
+	public void editar(String Nome, String CPF) {
+		super.editar(Nome, CPF);
 	}
-	public void setNome(String nome) {
-		Nome = nome;
-	}
-	public String getCPF() {
-		return CPF;
-	}
-	public void setCPF(String cPF) {
-		CPF = cPF;
-	}
-
-	public void Cadastrar_Dentista() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("digite o nome do dentista");
-		Nome = sc.nextLine();
-		System.out.println("digite o CPF do dentista");
-		CPF = sc.nextLine();
-	}
+	
 	
 }
