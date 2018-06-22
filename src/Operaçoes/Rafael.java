d9a924c295555651b5ee3868a027cef045a33f35
Rafael Riegel <Rafael Riegel@191.52.29.54>
2018-06-22 18:50:56 -0300

    Inicio dos Metodos Fatura e Dentista

--- src/Operaçoes/Rafael.java
+++ src/Operaçoes/Rafael.java
@@ -1,21 +1,36 @@
+
 package src.Operaçoes;
+import src.Usuarios.*;
+import src.Operaçoes.*;
 
 public class Rafael {
 	
 	
-public void listarDentista() {
-	for(int i = 0;  i < 100; i++) {
-}
-}
-public void listarFaturas() {
-	for(int i = 0;  i < 100; i++) {
-		
-}
-}
-public static void main(String args[]) {
-	Rafael metodos2 = new Rafael();
-	metodos2.listarDentista(); 
-	Rafael metodos3 = new Rafael();
-	metodos3.listarFaturas(); 
-}
+	private Dentista dentistas[];
+	private int indiceDentistas=0;
+	
+	public void listarDentista() {
+		for(int p = 0;  p < indiceDentistas; p++) {
+			if(dentistas[p] != null) {
+				System.out.println(p + " " + dentistas[p]);
+			}
+		}
+	}
+	public static void main(String[] arg) {
+		Rafael metodos2 = new Rafael();
+		metodos2.listarDentista();
+	}
+	private Fatura faturas[];
+	private int indiceFaturas=0;
+	
+	public void listarFatura() {
+		for(int v = 0;  v < indiceFaturas; v++) {
+			if(faturas[v] != null) {
+				System.out.println(v + " " + faturas[v]);
+				
+				
+			}
+			
+		}
+	}
 }
